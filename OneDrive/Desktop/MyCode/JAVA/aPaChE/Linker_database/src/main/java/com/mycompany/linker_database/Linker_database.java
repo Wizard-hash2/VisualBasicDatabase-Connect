3c51ab1a192b53930784ecb247988cc7b3ef392f
wizard-hash <aronidengeno@gmail.com>
2024-10-30 13:03:50 +0300

    My dbms connector

---
+++ OneDrive/Desktop/MyCode/JAVA/aPaChE/Linker_database/src/main/java/com/mycompany/linker_database/Linker_database.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.linker_database;
+
+/**
+ *
+ * @author Aron
+ */
+public class Linker_database {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
